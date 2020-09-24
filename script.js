10fc92135c1cb5424a45e4ef63f4d0765a9c7ffd
wolyslager <olyslager.willy@gmail.com>
2020-09-24 10:45:41 -0500

    created initial layout

---
+++ script.js
@@ -0,0 +1,8 @@
+const table = document.querySelector('.table');
+const newRow = document.createElement('tr');
+const newCell = document.createElement('td')
+
+
+table.insertRow(-1);
+
+console.log(table);
